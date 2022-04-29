40a45269d7471fe64c72dc1168ab87dbb5ba6eeb
tngamer101 <103201714+tngamer101@users.noreply.github.com>
2022-04-29 21:16:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://tngaming:tngaming@cluster0.fjk1h.mongodb.net/tngaming?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5394744068:AAHekszQY3AhkB-jG6-u-hlbaEq2SIXaliE',//bot token
+    
+    bot_name:'CRYPTO_VERSE_AIRDROPBOT',//bot username without @
+    
+    bot_admin:1996486622,// admin telegram id
+    
+    channelsList : ['@OfficialEarthQuake','@Crypto_verse1','@Airdropspanther','@Steel_Dragons','@PakAirdropsAler','@ATTITUDEARNERS','@TNGairdrop','@airdrop_raja'],// channels list
+    
+    reffer_bonus:1000000,// refferal bonus amount
+    
+    min_wd:2000000,//min withdrawal amount
+    
+    daily_bonus:100000,// daily bonus amount
+    
+    currency:'TNG', //bot cuurency
+    
+    cb_api_key:'1996486622',//coinbase api key
+    
+    cb_api_secret:'SHSJJSJSK',// coinbase secret key
+    
+    cb_account_id:'OPMSBSK',// counbase account id for the bot currency
+    
+    payment_channel:'@payoutworldPpp'// payment channel username
+    
+    }
